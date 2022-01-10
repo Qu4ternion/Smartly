60efdadd8fbe61c006743a6ab9d9d5270453171d
Qu4artenion <gossony.reda@gmail.com>
2022-01-10 16:30:17 +0100

    Ajout des métriques statistiques

--- main.py
+++ main.py
@@ -3,16 +3,24 @@
 Created on Mon Jan 10 13:08:58 2022
 
 @author: Acer
+
+
+###################################################
+# Point d'entrée du programme:                    #
+#    Lancez avec "streamlit run main.py" sur CLI. #
+###################################################
 """
 
-import streamlit as st
-from preprocessing import Preprocessor
-from tf_idf import TFIDF
-from model import kmeans
-from metrics import Metrics
-from io import StringIO 
+import streamlit         as st
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+import json
+from   preprocessing   import Preprocessor
+from   tf_idf          import TFIDF
+from   model           import kmeans
+from   metrics         import Metrics
+from   io              import StringIO 
+from   sklearn.cluster import KMeans
+from   collections     import Counter
 
 
 # Add title:
@@ -50,6 +58,7 @@ if file is not None:
     # Metrics 
     st.subheader('Metrics:')
     
+    # Empty list to store inertia:
     Sum_of_squared_distances = []
     K = range(2,10)
     for k in K:
@@ -64,3 +73,25 @@ if file is not None:
     ax.set_ylabel('Sommes des carrés')
     ax.set_title('Méthode du coude pour nombre optimal de groupes')
     st.pyplot(fig)
+
+    
+    st.subheader('Fréquence des mots:')
+    
+    fig, ax = plt.subplots()
+    
+    # Dictionary that holds clusters and words in each cluster:
+    d = json.loads(clustering)
+    
+    
+    
+    for cluster in d.keys():
+        
+        word_counts = Counter(d[cluster])
+        word_counts = {k: v for k, v in sorted(word_counts.items(), key=lambda item: item[1], reverse=True)}
+        
+        fig, ax = plt.subplots()
+        ax.bar(list(word_counts.keys())[0:5], list(word_counts.values())[0:5], color ='maroon',width = 0.4)
+        ax.set_xlabel('Mots les plus fréquents')
+        ax.set_ylabel('Fréquence')
+        ax.set_title(f'Cluster {cluster}')
+        st.pyplot(fig)
